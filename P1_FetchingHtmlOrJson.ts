bc012bac445cc7bd2cd0310d1d171595943e5a92
Sahil <sahilrajput03@gmail.com>
2020-05-23 13:27:50 +0530

    Update P1_FetchingHtmlOrJson.ts

--- P1_FetchingHtmlOrJson.ts
+++ P1_FetchingHtmlOrJson.ts
@@ -2,4 +2,5 @@ const fetched = await fetch("https://sahilrajput03.github.io/").then((t) =>
   t.text()
 );
 // for fetching json, you could do t.json() instead.
-console.log("$log:Html fetched:-", fetched);
+console.log("$log:Html fetched:-", fetched);P
+console.log("Program Ends..")
